f89ba6fae3dfb517753479073a535bbc2e2caa96
naniwijaya20 <naniwijayahh291199@gmail.com>
2021-08-13 00:41:26 +0800

    naniwijayahhh

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@
 */
 
 use App\Http\Controllers\pembelianController;
+use App\Http\Controllers\SupplierAPIController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -24,6 +25,13 @@ Route::resource('barang','BarangController');
 Route::resource('pelanggan','pelangganController');
 Route::resource('supplier','supplierController');
 Route::resource('pembelian','pembelianController');
+Route::resource('laporanpenjualan','LaporanPenjualanController');
+Route::resource('laporanpembelian','LaporanPembelianController');
+
+Route::get('laporan','Laporan1Controller@index');
+Route::get('laporan-tanggal','Laporan1Controller@tanggal');
+Route::get('laporan1','Laporan1Controller@pdf');
+Route::get('chart');
 
 
 Auth::routes();
