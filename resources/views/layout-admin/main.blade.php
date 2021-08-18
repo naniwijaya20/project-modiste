ffcbf2190596cd73d7394cb3be044013bea17573
naniwijaya20 <naniwijayahh291199@gmail.com>
2021-08-18 10:51:53 +0800

    naniwijayahh

--- resources/views/layout-admin/main.blade.php
+++ resources/views/layout-admin/main.blade.php
@@ -108,8 +108,8 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-          <div class="logo-pro">
-            <a href="index.html"><img class="main-logo" src="img/logo/logo.png" alt="" /></a>
+          <div class="logo-pro" style="margin:10px 10px 10px 10px" >
+            <a href="index.html">
           </div>
         </div>
       </div>
